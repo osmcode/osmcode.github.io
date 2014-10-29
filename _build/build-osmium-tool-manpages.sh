a1ae793c57cf7fcb03c3a6fd82a4e7b202342e01
Jochen Topf <jochen@topf.org>
2014-10-29 13:43:30 +0100

    Improved display of manual pages for Osmium tool.

--- _build/build-osmium-tool-manpages.sh
+++ _build/build-osmium-tool-manpages.sh
@@ -9,6 +9,11 @@ DIR=`pwd`
 cd $MANUAL_DIR
 
 for i in *.md; do
-    pandoc -s -t html -o $DIR/osmium/man/${i%%.md}.html $i
+    OUT="$DIR/osmium/man/${i%%.md}.html"
+    TITLE=`head -1 $i | sed -e 's/% //'`
+    echo "---\nlayout: default\ntitle: $TITLE - Osmium Manpages\n---" >$OUT
+    echo "&uarr; <a href='..'>Osmium</a>" >>$OUT
+    echo "<h1 class='title'>$TITLE</h1>" >>$OUT
+    sed -e 's/^#/##/' $i | pandoc -t html - >>$OUT
 done
 
